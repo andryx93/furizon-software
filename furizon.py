dd2e4cd1bc5c4a4005226fb4ed160896f9a1afd8
Andrea Cavallaro <andryx93@gmail.com>
2020-07-12 18:38:54 +0200

    - master.html improved - index.html improved - add error pages - add error handles in furizon.py

--- furizon.py
+++ furizon.py
@@ -2,7 +2,48 @@ from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+
+"""
+   SUMMARY
+      1. ERROR PAGES
+         - 404
+         - 500
+         - 503
+      2. MAIN ENTRY POINTS (index, login)
+         - / (index)
+
+
+"""
+
+
+"""
+   1. ERROR PAGES
+"""
+@app.errorhandler(404)
+def error404(e):
+   return render_template("404.html"),404
+
+@app.errorhandler(500)
+def error500(e):
+   return render_template("500.html"),500
+
+@app.errorhandler(503)
+def error503(e):
+   return render_template("503.html"),503
+"""
+   --- END ---
+"""
+
+
+"""
+   2. MAIN ENTRY POINTS
+"""
 @app.route('/')
 def index():
-   return render_template("index.html")
+   name = "Kyrill"
+   return render_template("index.html", name=name)
 
+
+"""
+   --- END ---
+"""
